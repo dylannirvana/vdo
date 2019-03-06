c2931b89609039381a967089b7ab64f524a8a055
Dylan Nirvana <dylan@dylannirvana.com>
2019-03-06 09:15:07 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,18 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import { Player } from 'video-react'
+import "../node_modules/video-react/dist/video-react.css"; // import css
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <Player
+      playsInline
+      poster="/assets/poster.png"
+      src="./assets/IMG_0059.m4v"
+    />
+      
+      
     );
   }
 }
